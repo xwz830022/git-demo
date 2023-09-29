914a7d595f2eb5fb0dded52b894e314366f86573
xwz830022 <xwz1992622@outlook.com>
2023-09-29 21:38:29 +0200

    Change Test

--- src/test/java/com/ws/mybatis/test/MybatisTest.java
+++ src/test/java/com/ws/mybatis/test/MybatisTest.java
@@ -15,43 +15,43 @@ import java.util.List;
 
 public class MybatisTest {
 
-    @Test
-    public void testInsert() throws IOException {
-        InputStream is = Resources.getResourceAsStream("mybatis_config.xml");
-        SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
-        SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(is);
-        SqlSession sqlSession = sqlSessionFactory.openSession(true);
-        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
-        int result = userMapper.insertUser();
-        System.out.println("result: " + result);
-        //sqlSession.commit();
-        sqlSession.close();
-    }
-
-    @Test
-    public void testUpdate() {
-        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
-        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
-        userMapper.updateUser();
-        sqlSession.close();
-    }
-
-    @Test
-    public void deleteUpdate() {
-        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
-        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
-        userMapper.deleteUser();
-        sqlSession.close();
-    }
-
-    @Test
-    public void testGetById() {
-        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
-        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
-        User user = userMapper.getUserById();
-        System.out.println(user);
-        sqlSession.close();
-    }
+//    @Test
+//    public void testInsert() throws IOException {
+//        InputStream is = Resources.getResourceAsStream("mybatis_config.xml");
+//        SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+//        SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(is);
+//        SqlSession sqlSession = sqlSessionFactory.openSession(true);
+//        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+//        int result = userMapper.insertUser();
+//        System.out.println("result: " + result);
+//        //sqlSession.commit();
+//        sqlSession.close();
+//    }
+//
+//    @Test
+//    public void testUpdate() {
+//        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+//        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+//        userMapper.updateUser();
+//        sqlSession.close();
+//    }
+//
+//    @Test
+//    public void deleteUpdate() {
+//        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+//        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+//        userMapper.deleteUser();
+//        sqlSession.close();
+//    }
+//
+//    @Test
+//    public void testGetById() {
+//        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+//        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+//        User user = userMapper.getUserById();
+//        System.out.println(user);
+//        sqlSession.close();
+//    }
 
     @Test
     public void testGetAllUsers() {
